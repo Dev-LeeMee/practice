b3797b38c8ce286c3378f286a80c776e7c58ed47
Lee Mee <dev.leemee@gmail.com>
2021-08-03 23:31:13 +0900

    모두를위한파이썬 Chapter5. 3주차 미션. Quiz1. 연습_adding a tip (how to make the line6 simpler)

--- practice_python/mission3_Q1.py
+++ practice_python/mission3_Q1.py
@@ -4,3 +4,6 @@ for turns in times:
     answer = number * turns
     if answer<50:
         print(number, 'x', turns, '=', answer)
+
+#마지막 라인(line6) 더 심플하게 만드는 방법
+#print(f'{number} x {turns} = {answer}')
