6ba148195ed41777f196bd6c532a5db80b1aaab1
Lee Mee <dev.leemee@gmail.com>
2021-08-09 15:57:26 +0900

    모두를위한파이썬 Chapter7.file(opening a file and counting lines+indent)_연습1-1

--- practice_python/ch7_file_practice1.py
+++ practice_python/ch7_file_practice1.py
@@ -2,4 +2,13 @@ xfile=open('FOR LOOP practice.py')
 count=0
 for line in xfile:
     count=count+1
+    print('Line Count: ', count)
+
+#indent: 위와 같이 하면 라인별로 다 프린트 되고
+#아래와 같이 하면 총 라인 갯수만 프린트 됨.
+
+xfile=open('FOR LOOP practice.py')
+count=0
+for line in xfile:
+    count=count+1
 print('Line Count: ', count)
